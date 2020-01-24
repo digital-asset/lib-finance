c4322aaa5a48f2db5b67e2d531ad91c82e0927bd
Thomas Weber <thomas.weber@digitalasset.com>
2020-01-24 08:59:08 +0100

    More trigger tests

--- daml-test/scripts/start-processes.sh
+++ daml-test/scripts/start-processes.sh
@@ -9,6 +9,7 @@ PARTIES=("Alice" "Charlie" "AcmeBank" "GencoBank")
 REUTERS_TRIGGERS=("Test.Trigger.Finance.Main:entitlementTrigger"
                   "Test.Trigger.Finance.Main:equityTrigger"
                  )
+TEST_SCRIPT="Test.Trigger.Finance.TestScript:test"
 
 cd `dirname $0`
 cd ..
@@ -18,12 +19,14 @@ do
   for PARTY in ${PARTIES[@]}
   do
     echo "Starting $TRIGGER for $PARTY"
-    daml trigger --dar .daml/dist/finlib-test-1.0.0.dar --trigger-name $TRIGGER --ledger-host "localhost" --ledger-port 6865 --ledger-party $PARTY &
+    daml trigger --dar .daml/dist/finlib-test-1.0.0.dar -w --trigger-name $TRIGGER --ledger-host "localhost" --ledger-port 6865 --ledger-party $PARTY &
   done
 done
 
 for TRIGGER in ${REUTERS_TRIGGERS[@]}
 do
   echo "Starting $TRIGGER for Reuters"
-  daml trigger --dar .daml/dist/finlib-test-1.0.0.dar --trigger-name $TRIGGER --ledger-host "localhost" --ledger-port 6865 --ledger-party Reuters &
+  daml trigger --dar .daml/dist/finlib-test-1.0.0.dar -w --trigger-name $TRIGGER --ledger-host "localhost" --ledger-port 6865 --ledger-party Reuters &
 done
+
+daml script --dar .daml/dist/finlib-test-1.0.0.dar -w --script-name $TEST_SCRIPT --input-file "scripts/config.json" --ledger-host "localhost" --ledger-port 6865
--- daml-test/scripts/test-trigger.sh
+++ daml-test/scripts/test-trigger.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 
 DIR=$(dirname $0)
-daml start --start-navigator "no" --sandbox-option "-w" &
 
-sleep 5
-echo "Timed out"
-exit -1
+function local {
+  daml start --start-navigator "no" --sandbox-option "-w" --on-start "$DIR/start-processes.sh"
+}
+
+function circleci {
+  rm -f test.log
+  daml start --start-navigator "no" --sandbox-option "-w" --on-start "$DIR/start-processes.sh" 2>&1 | tee test.log &
+
+  # Run until 'Test finished' is found in logs
+  while [ true ]; do
+    echo "Running.."
+    result=$(grep -F 'TestScript Finished' test.log)
+    if [ "$result" ] ; then
+      echo "Test finished"
+      break
+    fi
+
+    sleep 10
+  done
+  exit 0
+}
+
+case $1 in
+
+  local)
+    local
+    ;;
+  circleci)
+    circleci
+    ;;
+  *)
+    echo "argument 'local' or 'cirecleci' rqeuired"
+    ;;
+esac
+
+
