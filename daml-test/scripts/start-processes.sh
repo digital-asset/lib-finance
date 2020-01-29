b2a99aec4291b5da121de7a82af646808db3162b
Thomas Weber <thomas.weber@digitalasset.com>
2020-01-29 18:15:53 +0100

    Combine triggers

--- daml-test/scripts/start-processes.sh
+++ daml-test/scripts/start-processes.sh
@@ -1,32 +1,20 @@
 #!/bin/bash
  
-PARTY_TRIGGERS=("Test.Trigger.Finance.Main:assetLifecycleTrigger"
-                "Test.Trigger.Finance.Main:instructionTrigger"
-                "Test.Trigger.Finance.Main:dvpLifecycleTrigger"
-                "Test.Trigger.Finance.Main:dvpSettlementTrigger"
-                )
 PARTIES=("Alice" "Charlie" "AcmeBank" "GencoBank")
-REUTERS_TRIGGERS=("Test.Trigger.Finance.Main:entitlementTrigger"
-                  "Test.Trigger.Finance.Main:equityTrigger"
-                 )
+PARTY_TRIGGER="Test.Trigger.Finance.Main:partyTrigger"
+REUTERS_TRIGGER="Test.Trigger.Finance.Main:reutersTrigger"
 TEST_SCRIPT="Test.Trigger.Finance.TestScript:test"
 
 cd `dirname $0`
 cd ..
 
-for TRIGGER in ${PARTY_TRIGGERS[@]}
+for PARTY in ${PARTIES[@]}
 do
-  for PARTY in ${PARTIES[@]}
-  do
-    echo "Starting $TRIGGER for $PARTY"
-    daml trigger --dar .daml/dist/finlib-test-1.0.0.dar -w --trigger-name $TRIGGER --ledger-host "localhost" --ledger-port 6865 --ledger-party $PARTY &
-  done
+  echo "Starting trigger for $PARTY"
+  daml trigger --dar .daml/dist/finlib-test-1.0.0.dar -w --trigger-name $PARTY_TRIGGER --ledger-host "localhost" --ledger-port 6865 --ledger-party $PARTY &
 done
 
-for TRIGGER in ${REUTERS_TRIGGERS[@]}
-do
-  echo "Starting $TRIGGER for Reuters"
-  daml trigger --dar .daml/dist/finlib-test-1.0.0.dar -w --trigger-name $TRIGGER --ledger-host "localhost" --ledger-port 6865 --ledger-party Reuters &
-done
+echo "Starting trigger for Reuters"
+daml trigger --dar .daml/dist/finlib-test-1.0.0.dar -w --trigger-name $REUTERS_TRIGGER --ledger-host "localhost" --ledger-port 6865 --ledger-party Reuters &
 
 daml script --dar .daml/dist/finlib-test-1.0.0.dar -w --script-name $TEST_SCRIPT --input-file "scripts/config.json" --ledger-host "localhost" --ledger-port 6865
