12715adba5b36199c03e8a315f7a99f3282595c1
Thomas Weber <thomas.weber@digitalasset.com>
2020-01-30 15:06:31 +0100

    Fix version number in script

--- daml-test/scripts/start-processes.sh
+++ daml-test/scripts/start-processes.sh
@@ -11,10 +11,10 @@ cd ..
 for PARTY in ${PARTIES[@]}
 do
   echo "Starting trigger for $PARTY"
-  daml trigger --dar .daml/dist/finlib-test-1.0.0.dar -w --trigger-name $PARTY_TRIGGER --ledger-host "localhost" --ledger-port 6865 --ledger-party $PARTY &
+  daml trigger --dar .daml/dist/finlib-test-2.0.0.dar -w --trigger-name $PARTY_TRIGGER --ledger-host "localhost" --ledger-port 6865 --ledger-party $PARTY &
 done
 
 echo "Starting trigger for Reuters"
-daml trigger --dar .daml/dist/finlib-test-1.0.0.dar -w --trigger-name $REUTERS_TRIGGER --ledger-host "localhost" --ledger-port 6865 --ledger-party Reuters &
+daml trigger --dar .daml/dist/finlib-test-2.0.0.dar -w --trigger-name $REUTERS_TRIGGER --ledger-host "localhost" --ledger-port 6865 --ledger-party Reuters &
 
-daml script --dar .daml/dist/finlib-test-1.0.0.dar -w --script-name $TEST_SCRIPT --input-file "scripts/config.json" --ledger-host "localhost" --ledger-port 6865
+daml script --dar .daml/dist/finlib-test-2.0.0.dar -w --script-name $TEST_SCRIPT --input-file "scripts/config.json" --ledger-host "localhost" --ledger-port 6865
