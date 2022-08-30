66329b2cd3f51139ff90af99973c4178b29a391b
Matteo Limberto <matteo.limberto@digitalasset.com>
2022-08-30 15:20:08 +0200

    update sdk to 2.3.2 on syndication branch (#28)
    * update sdk to 2.3.2
    
    * Update trigger test to use users instead of parties
    
    * pass users in configuration (in order to avoid the `TestScript` being run as part of `daml test`, which leads to a timeout as triggers are not active then)

--- test/scripts/test-trigger.sh
+++ test/scripts/test-trigger.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-PARTIES=("Alice" "Charlie" "AcmeBank" "GencoBank")
+USERS=("alice" "charlie" "acmebank" "gencobank")
 PARTY_TRIGGER="Test.Trigger.Finance.Main:partyTrigger"
 REUTERS_TRIGGER="Test.Trigger.Finance.Main:reutersTrigger"
+SETUP_SCRIPT="Test.SetupScript:setup"
 TEST_SCRIPT="Test.Trigger.Finance.TestScript:test"
 
 function waitForPortfile {
@@ -15,33 +16,39 @@ function waitForPortfile {
 
 DIR=$(dirname $0)
 
-# Start and wati for sandbox
+# Start and wait for sandbox
 rm -f .daml/portfile.txt
-daml sandbox -w .daml/dist/finlib-test-2.0.0.dar --port-file .daml/portfile.txt &
+daml sandbox --dar .daml/dist/finlib-test-2.0.1.dar --port-file .daml/portfile.txt --wall-clock-time &
 SANDBOX_PID=$!
 waitForPortfile
+
+# Run setup script to allocate users and parties
+export _JAVA_OPTIONS="-Xms256m -Xmx1g"
+daml script --dar .daml/dist/finlib-test-2.0.1.dar -w --script-name $SETUP_SCRIPT --ledger-host "localhost" --ledger-port 6865
+STATUS=$?
 
 #Start triggers
 export _JAVA_OPTIONS="-Xms64m -Xmx256m"
 
 TRIGGER_PARTY_PIDS=()
-for PARTY in ${PARTIES[@]}
+for USER in ${USERS[@]}
 do
-  echo "Starting trigger for $PARTY"
-  daml trigger --dar .daml/dist/finlib-test-2.0.0.dar -w --trigger-name $PARTY_TRIGGER --ledger-host "localhost" --ledger-port 6865 --ledger-party $PARTY &
+  echo "Starting trigger for $USER"
+  daml trigger --dar .daml/dist/finlib-test-2.0.1.dar -w --trigger-name $PARTY_TRIGGER --ledger-host "localhost" --ledger-port 6865 --ledger-user $USER &
   TRIGGER_PARTY_PIDS+=($!)
 done
 
 echo "Starting trigger for Reuters"
-daml trigger --dar .daml/dist/finlib-test-2.0.0.dar -w --trigger-name $REUTERS_TRIGGER --ledger-host "localhost" --ledger-port 6865 --ledger-party Reuters &
+daml trigger --dar .daml/dist/finlib-test-2.0.1.dar -w --trigger-name $REUTERS_TRIGGER --ledger-host "localhost" --ledger-port 6865 --ledger-user reuters &
 TRIGGER_REUTERS_PID=$!
 
 #Start script
 export _JAVA_OPTIONS="-Xms256m -Xmx1g"
-daml script --dar .daml/dist/finlib-test-2.0.0.dar -w --script-name $TEST_SCRIPT --input-file "scripts/config.json" --ledger-host "localhost" --ledger-port 6865
+daml script --dar .daml/dist/finlib-test-2.0.1.dar -w --script-name $TEST_SCRIPT --ledger-host "localhost" --ledger-port 6865 --input-file "scripts/config.json"
 STATUS=$?
 
 #kill all processes
+echo "Shutting processes down"
 kill $TRIGGER_REUTERS_PID
 for PID in ${TRIGGER_PARTY_PIDS[@]}
 do kill $PID
